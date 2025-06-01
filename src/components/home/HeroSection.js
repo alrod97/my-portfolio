b8d0e04068345e48e4de518bc9889b5fe59f2f97
alrod97 <alberto.rodriguez.salgado.97@gmail.com>
2025-06-01 17:49:09 +0200

    update profile pic

--- src/components/home/HeroSection.js
+++ src/components/home/HeroSection.js
@@ -15,36 +15,34 @@ const HeroSection = () => {
               <span className="text-accent">Deep Learning</span> & <span className="text-accent">Computer Vision</span> Engineer
             </h1>
             <p className="text-lg mb-8 max-w-2xl text-gray-700" style={{ lineHeight: '1.6' }}>
-              At <span className="font-semibold text-primary">Volkswagen AG</span>, I develop
-              <span className="font-semibold text-accent"> AI-driven automotive solutions</span>, leveraging
+              At <span className="font-semibold text-primary">Volkswagen AG</span>, I specialize in
+              <span className="font-semibold text-accent"> translating state-of-the-art research</span> into
+              <span className="font-semibold text-accent"> real-world AI solutions</span>, focusing on
               <span className="font-semibold text-accent"> deep learning</span>,
-              <span className="font-semibold text-accent"> 3D computer vision</span>,
-              <span className="font-semibold text-accent"> spatial computing</span>, and
-              <span className="font-semibold text-accent"> graphics</span> to build
-              <span className="font-semibold"> immersive</span>,
-              <span className="font-semibold"> high-performance</span> experiences.
+              <span className="font-semibold text-accent"> computer vision</span>, and
+              <span className="font-semibold text-accent"> 3D generative AI</span> for automotive applications.
             </p>
             <p className="text-lg mb-8 max-w-2xl text-gray-700" style={{ lineHeight: '1.6' }}>
-              My projects range from <span className="font-semibold text-accent">mixed reality applications</span> with
-              <span className="font-semibold text-primary"> Apple Vision Pro</span> to enhancing
-              <span className="font-semibold text-primary"> Vision Transformers</span> for
-              <span className="font-semibold text-accent"> autonomous driving</span> alongside
-              <span className="font-semibold text-primary"> NVIDIA</span>.
+              I develop innovative methods like <span className="font-semibold text-accent">CWFA (Channel Wise Feature Augmentation) </span>
+               under <span className="font-semibold text-primary">Dr. Jose M Alvarez (NVIDIA)</span> supervision,
+              enhancing <span className="font-semibold text-primary">SegFormer's robustness</span> to
+              <span className="font-semibold text-accent">out-of-distribution data</span> for semantic segmentation.
+              I've also created <span className="font-semibold text-accent">mixed reality solutions</span> like the
+              <span className="font-semibold text-primary"> CUPRA Vision Pro</span> car configuration platform.
             </p>
             <p className="text-lg max-w-2xl text-gray-700" style={{ lineHeight: '1.6' }}>
-              I’m passionate about creating
-              <span className="font-semibold text-primary"> impactful solutions</span> that merge
-              <span className="font-semibold text-accent"> AI</span>,
-              <span className="font-semibold text-accent"> spatial computing</span>, and
-              <span className="font-semibold text-accent"> computer graphics</span>. My approach emphasizes
-              <span className="font-semibold text-primary"> practical innovation</span>—turning ambitious ideas into
-              <span className="font-semibold text-primary"> real-world products</span>.
+              Currently, I focus on <span className="font-semibold text-accent">3D generative AI</span> for
+              <span className="font-semibold text-primary">internal design use cases</span>, researching
+              <span className="font-semibold text-accent"> cutting-edge architectures</span> and
+              <span className="font-semibold text-primary"> fine-tuning models</span> to meet specific industrial needs,
+              bridging the gap between <span className="font-semibold text-accent">academic innovation</span> and
+              <span className="font-semibold text-primary"> practical implementation</span>.
             </p>
           </div>
           <div className="md:w-1/3 md:pl-12 flex items-center justify-center">
             <div className="w-64 overflow-hidden rounded-md shadow-lg">
               <img
-                src="https://media.licdn.com/dms/image/v2/D4D03AQG8lvgDda3Mpg/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1724171213849?e=1747267200&v=beta&t=lOZBC6fE5OARDrMvZeMT3IwVmNFXk92eRKHmmNz1yqs"
+                src={`${process.env.PUBLIC_URL}/images/profile_image.JPG`}
                 alt="Portrait"
                 className="w-full h-auto object-cover"
               />
